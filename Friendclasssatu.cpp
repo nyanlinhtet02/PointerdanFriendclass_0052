01d74f43eb6c58747f552c7365b6e2c42fa2f3ce
nyanlinhtet02 <nyan.lin.ft24@mail.umy.ac.id>
2025-05-05 05:17:18 -0700

    declaring class

---
+++ Friendclasssatu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int Id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << Id << endl
+         << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    Id = pId;
+}
+
+int main()
+{
+    orang joko;
+    joko.setNama("joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
---
+++ PointerDua.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout << "no induk = "<<nim<<endl;
+    }
+};
+
+int main(){
+    mahasiswa *mhs = new mahasiswa{1};//pointer objek mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
